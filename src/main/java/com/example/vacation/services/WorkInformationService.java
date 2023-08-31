5644f1fdec08954424d7e1ebfc4baa6a53f11230
seralv <sergioalvaro.14@gmail.com>
2023-08-30 22:51:29 -0400

    Adding some function to...

--- src/main/java/com/example/vacation/services/WorkInformationService.java
+++ src/main/java/com/example/vacation/services/WorkInformationService.java
@@ -1,5 +1,6 @@
 package com.example.vacation.services;
 
+import com.example.vacation.DTO.WorkInformationDTO;
 import com.example.vacation.models.WorkInformation;
 import com.example.vacation.repository.WorkInformationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,5 +16,11 @@ public class WorkInformationService {
 
     public List<WorkInformation> getAllWorkInformation() {
         return workInformationRepository.findAll();
+    }
+
+    public void createWorkInformation(WorkInformationDTO workInformationDTO) {
+//        WorkInformation wi =  WorkInformation.builder()
+//                .da
+//                .build();
     }
 }
